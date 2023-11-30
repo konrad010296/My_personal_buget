c93dff5ce62d0150d5d672887088fb1454583692
Konrad <konradskolimowski1996@interia.pl>
2023-11-30 13:41:23 +0100

    Completed class User

--- User.cpp
+++ User.cpp
@@ -1,1 +1,33 @@
 #include "User.h"
+
+void User::setUserId(int id){
+    userId = id;
+}
+void User::setUserName(string name){
+    userName = name;
+}
+void User::setUserSurname(string surname){
+    userSurname = surname;
+}
+void User::setUserLogin(string login){
+    userLogin = login;
+}
+void User::setUserPassword(string password){
+    userPassword = password;
+}
+
+int User::getUserId(){
+    return userId;
+}
+string User::getUserName(){
+    return userName;
+}
+string User::getUserSurname(){
+    return userSurname;
+}
+string User::getUserLogin(){
+    return userLogin;
+}
+string User::getUserPassword(){
+    return userPassword;
+}
