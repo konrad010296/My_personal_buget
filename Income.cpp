51930bfb8fa4065b31ccef7d7b8718421a2901ba
Konrad <konradskolimowski1996@interia.pl>
2023-11-30 13:45:08 +0100

    Completed class Income

--- Income.cpp
+++ Income.cpp
@@ -1,1 +1,33 @@
 #include "Income.h"
+
+void Income::setIncomeId(int id){
+incomeId = id;
+}
+void Income::setUserId(int idUser){
+userId = idUser;
+}
+void Income::setDate(string datee){
+date = datee;
+}
+void Income::setItemName(string itemName){
+item = itemName;
+}
+void Income::setAmount(float itemAmoutn){
+amount = itemAmoutn;
+}
+
+int Income::getIncomeId(){
+return incomeId;
+}
+int Income::getUserId(){
+return userId;
+}
+string Income::getDate(){
+return date;
+}
+string Income::getItemName(){
+return item;
+}
+float Income::getAmount(){
+return amount;
+}
--- Income.h
+++ Income.h
@@ -5,6 +5,23 @@
 using namespace std;
 
 class Income{
+    int incomeId;
+    int userId;
+    string date;
+    string item;
+    float amount;
 
+public:
+void setIncomeId(int id);
+void setUserId(int idUser);
+void setDate(string datee);
+void setItemName(string itemName);
+void setAmount(float itemAmoutn);
+
+int getIncomeId();
+int getUserId();
+string getDate();
+string getItemName();
+float getAmount();
 };
 #endif
